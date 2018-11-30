56159f9acb1b38a85ab80550edfdd5fe0ff6f468
andee99 <andeerakut14@gmail.com>
2018-11-30 10:33:21 +0100

    nanala kitty cat

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'home/productdetail', as: "detail"
-  get 'home/team', as: "team"
-  root 'home#index'
-  get 'home/index'
-  get 'home/login', as: "login"
-  get 'home/product', as: "product"
-  get 'home/checkout', as: "contact"
-=======
  root to: 'homes#index'
->>>>>>> f29f00c2be9fc439a387a37cc3bc3100a1dcccd4
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
